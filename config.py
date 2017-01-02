7c4acd3ad24658823d0e4ae51141a177f50c116e
Kevin Qi <iqnivek@gmail.com>
2017-01-02 14:20:05 -0800

    Remove warning. Also finishes part 4

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+SQLALCHEMY_TRACK_MODIFICATIONS = True  # disable warning for now
+
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
