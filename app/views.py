4a2232617b649f0122b6a94b19568664940736ce
Kevin Qi <iqnivek@gmail.com>
2017-01-01 17:45:31 -0800

    template for loop

--- app/views.py
+++ app/views.py
@@ -5,5 +5,20 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname': 'Kevin'}
+    posts = [
+        {
+            'author': {'nickname': 'John'},
+            'body': 'Beautiful day in portland!'
+        },
+        {
+            'author': {'nickname': 'Susan'},
+            'body': 'The movie was cool.'
+        }
+    ]
 
-    return render_template('index.html', title='Home', user=user)
+    return render_template(
+        'index.html',
+        title='Home',
+        user=user,
+        posts=posts
+    )
