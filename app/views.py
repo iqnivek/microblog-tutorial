fa72c4da71cbd26734aeb9f3fedc65286a989cb8
Kevin Qi <iqnivek@gmail.com>
2017-01-02 17:50:15 -0800

    WIP, add last_seen and about_me

--- app/views.py
+++ app/views.py
@@ -1,12 +1,17 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
 from app import app, db, lm
+from datetime import datetime
 from .forms import LoginForm
 from .models import User
 
 @app.before_request
 def before_request():
     g.user = current_user
+    if g.user.is_authenticated:
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
 
 @lm.user_loader
 def load_user(id):
