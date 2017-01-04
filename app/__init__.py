e273b393da6efb86b6359fae38ec3da72aee4a98
Kevin Qi <iqnivek@gmail.com>
2017-01-03 16:27:48 -0800

    Custom error pages, and non-working mail config

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from config import basedir, ADMINS, MAIL_SERVER, MAIL_PORT, MAIL_USERNAME, MAIL_PASSWORD
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -10,9 +11,15 @@ lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
 
-# import os
-# from config import basedir
-# from flask_openid import OpenID
-# oid = OpenID(app, os.path.join(basedir, 'tmp'))
+# mail server (not working?)
+if not app.debug:
+    import logging
+    from logging.handlers import SMTPHandler
+    credentials = None
+    if MAIL_USERNAME or MAIL_PASSWORD:
+        credentials = (MAIL_USERNAME, MAIL_USERNAME)
+    mail_handler = SMTPHandler((MAIL_SERVER, MAIL_PORT), 'no-reply@' + MAIL_SERVER, ADMINS, 'microblog failure', credentials)
+    mail_handler.setLevel(logging.ERROR)
+    app.logger.addHandler(mail_handler)
 
 from app import views, models
--- config.py
+++ config.py
@@ -2,16 +2,16 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_TRACK_MODIFICATIONS = True  # disable warning for now
-
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
-'''
-OPENID_PROVIDERS = [
-    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
-    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'}
-]
-'''
+# mail server
+# start with python -m smtpd -n -c DebuggingServer localhost:25
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+ADMINS = ['greg@example.com']
